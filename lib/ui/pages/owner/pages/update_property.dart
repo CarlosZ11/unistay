61c6353218a77c1e60470ff2e85de0862fd0727c
Dangello Galvis <dagalvis@unicesar.edu.co>
2025-04-15 13:12:16 -0500

    algunos cambios menores

--- lib/ui/pages/owner/pages/update_property.dart
+++ lib/ui/pages/owner/pages/update_property.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:shimmer/shimmer.dart';
